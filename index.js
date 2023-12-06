45b4cbde11b11e4a67eb7123405b7e3dacdc43b0
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2023-12-06 11:41:26 -0300

    Add Middlewares

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5001
 
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+app.use(express.static('public'))
+
+
 app.get('/' , (req , res)=>{
 
    res.send('hello from simple server :)')
