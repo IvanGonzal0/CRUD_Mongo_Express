fb6253cd9dc1d2b0855aae4c9ed3ed8f8e38af97
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2023-12-06 11:52:23 -0300

    Add get Routes

--- index.js
+++ index.js
@@ -3,16 +3,29 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5001
 
-// Middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended : true}))
-app.use(express.static('public'))
+// Middlewares (functions that execute when a certain route is hit)
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({extended : true}))  // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'))   // for serving static files
+app.use(cors()) // for cors policy
 
-
+// Routes
 app.get('/' , (req , res)=>{
 
    res.send('hello from simple server :)')
 
 })
 
+app.get("/api/v1/users", (req, res) => {
+
+    res.json({
+        msg: "hola mundo :)"
+    })
+});
+
+// app.post();
+// app.put();
+// app.delete();
+
+// Start the server
 app.listen(port , ()=> console.log('> Server is up and running on port : ' + port))
